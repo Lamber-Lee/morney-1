e70b35c0cad9f2e16913ee893a7b47eac4731936
Lamber <420862234@qq.com>
2023-03-02 23:03:46 +0800

    创建<Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+    <svg class="icon">
+      <use :xlink:href="'#'+name"/>
+    </svg>
+</template>
+
+<script lang="ts">
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try{
+  importAll(require.context('../assets/icons',true,/.svg$/));
+}catch(error){
+  console.log(error);
+}//try,catch是为了防止在单元测试中发生错误而停止的情况
+
+
+export default {
+  props: ['name'],
+  name: "Icon"
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em; height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
