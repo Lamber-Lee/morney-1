ab0ad8dd8922f2775a58526e744efcb3fed38669
Lamber <420862234@qq.com>
2023-03-04 12:43:48 +0800

    完成了局部å¤样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,5 @@
 <template>
-  <Layout class-prefix="xxx">
-    <div class="tags">
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-      <div class="new">
-        <button>新增标签</button>
-      </div>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注">
-      </label>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
+  <Layout class-prefix="layout">
     <div class="numberPad">
       <div class="output">100</div>
       <div class="buttons">
@@ -42,6 +19,29 @@
         <button>.</button>
       </div>
     </div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注">
+      </label>
+    </div>
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
   </Layout>
 </template>
 
@@ -52,11 +52,9 @@ export default {
 }
 </script>
 <style lang="scss">
-.xxx-wrapper{
-  border: 3px solid blue;
-}
-.xxx-content{
-  border: 3px solid red;
+.layout-content{
+  display: flex;
+  flex-direction: column-reverse;
 }
 </style>
 <style lang="scss" scoped>
@@ -155,9 +153,12 @@ padding-right: 16px;
 .tags {
   font-size: 14px;
   padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
   > .current {
     display: flex;
-
+    flex-wrap: wrap;
     > li {
       background: #d9d9d9;
       $h: 24px;
@@ -166,6 +167,7 @@ padding-right: 16px;
       border-radius: $h/2;
       padding: 0 16px;
       margin-right: 12px;
+      margin-top: 4px;
     }
   }
   .new{
