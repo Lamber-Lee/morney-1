c19dddbbfa63b1d6f4e4f0e0c07270694e4b0f6a
Lamber <420862234@qq.com>
2023-03-06 22:52:46 +0800

    修改了一些é拼写错误以及改造了 N欧çotes.vue

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,14 @@
 <style lang="scss">
 @import "~@/assets/style/helper.scss";
 @import "~@/assets/style/reset.scss";
-body{
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   line-height: 1.5;
   font-family: $font-hei;
   font-size: 16px;
+  background: #f5f5f5;
 }
 
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -14,7 +14,6 @@
   </Layout>
 </template>
 
-
 <script lang="ts">
 import Vue from "vue";
 import {Component} from "vue-property-decorator";
