bc38209e05fedaf8e2c8ab8c636bf06114e7ffb6
Lamber <420862234@qq.com>
2023-03-06 16:32:45 +0800

    收集四个组件的数据到 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -25,7 +25,8 @@ import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 @Component
 export default class NumberPad extends Vue {
-  output: string  = '0';
+  @Prop()readonly value!: number;
+  output = this.value.toString();
   inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
     const input = button.textContent!;
@@ -41,6 +42,7 @@ export default class NumberPad extends Vue {
     if (this.output.indexOf('.') >= 0 && input === '.') {return;}
     this.output += input;
   }
+
   remove() {
     if (this.output.length === 1) {
       this.output = '0';
@@ -52,6 +54,7 @@ export default class NumberPad extends Vue {
     this.output = '0';
   }
   ok() {
+    this.$emit('update:value',this.output);
   }
 }
 </script>
