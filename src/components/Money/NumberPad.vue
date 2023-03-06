a3060eb3b232e622d4d33d3b68c97be9c935d853
Lamber <420862234@qq.com>
2023-03-06 12:54:14 +0800

    å®基本完成 numberPad 组件ââ

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="numberPad">
-    <div class="output">{{ output }}</div>
+    <div class="output">{{output}}</div>
     <div class="buttons">
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok">OK</button>
+      <button @click="ok" class="ok">OK</button>
       <button @click="inputContent" class="zero">0</button>
       <button @click="inputContent">.</button>
     </div>
@@ -21,17 +21,37 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import {Component, Prop} from "vue-property-decorator";
-
-
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
 @Component
 export default class NumberPad extends Vue {
-  output = "";
-  inputContent(event: MouseEvent){
+  output: string  = '0';
+  inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
     const input = button.textContent!;
-
+    if (this.output.length === 16) { return; }
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+    this.output += input;
+  }
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+  clear() {
+    this.output = '0';
+  }
+  ok() {
   }
 }
 </script>
