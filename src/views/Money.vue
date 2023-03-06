f91b7983fdcaa098ff84ba3c931a1b6199212515
Lamber <420862234@qq.com>
2023-03-06 23:14:15 +0800

    Notes.vue 改成了 FormItem.vuee,修建Button.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注"
+    <FormItem field-name="备注"
            placeholder="在这里输入备注"
         @update: value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,7 +14,7 @@ import Vue, {onUpdated} from "vue";
 import {Component, Watch} from "vue-property-decorator";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
-import Notes from "@/components/Money/Notes.vue";
+import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import recordListModel from "@/models/recordListModel";
 import tagListModel from "@/models/tagListModel";
@@ -24,7 +24,7 @@ const tagList = tagListModel.fetch();
 
 
 @Component({
-  components: {Tags, Notes, Types, NumberPad},
+  components: {Tags, FormItem, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = tagList;
@@ -54,5 +54,8 @@ export default class Money extends Vue {
 .layout-content{
   display: flex;
   flex-direction: column-reverse;
+}
+.notes{
+  padding: 12px 0;
 }
 </style>
