9d31fdefad63d554f123cede35aa50fc4b611038
Lamber <420862234@qq.com>
2023-03-07 22:33:05 +0800

    使用 Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,13 +19,18 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from "vue-property-decorator";
-import store from '@/store/index2.ts';
+import oldStore from '@/store/index2.ts';
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad}
+  components: {Tags, FormItem, Types, NumberPad},
+  computed:{
+    recordList(){
+      return this.$store.state.count;
+    }
+  }
 })
 export default class Money extends Vue {
-  recordList = store.recordList;
+  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -33,7 +38,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    store.createRecord(this.record);
+    oldStore.createRecord(this.record);
   }
 }
 </script>
