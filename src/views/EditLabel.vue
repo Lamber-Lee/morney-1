81447c206be0fe4f28c47cc0fdaba1b5a1dd30f5
Lamber <420862234@qq.com>
2023-03-07 11:22:11 +0800

    修复 value被赋值的问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -40,7 +40,7 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.navBar{
+.navBar {
   text-align: center;
   font-size: 16px;
   padding: 12px 16px;
@@ -48,27 +48,24 @@ export default class EditLabel extends Vue {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  > .title{
-
+  > .title {
   }
-
   > .leftIcon {
     width: 24px;
     height: 24px;
   }
-
   > .rightIcon {
     width: 24px;
     height: 24px;
   }
-  .form-wrapper{
-    background: white;
-    margin-top: 8px;
-  }
-  .button-wrapper{
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
-  }
+}
+.form-wrapper{
+  background: white;
+  margin-top: 8px;
+}
+.button-wrapper{
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
 }
 </style>
